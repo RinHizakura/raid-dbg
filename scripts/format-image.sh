fbb1802d01eb62a498a569aef7a198aad9168464
RinHizakura <s921975628@gmail.com>
2022-03-31 00:38:55 +0800

    Remove useless scripts

--- scripts/format-image.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-rm -rf disk.img
-dd if=/dev/zero of=disk.img bs=4096 count=1024
-sudo losetup -fP disk.img
-loopdev=$(losetup -a | grep disk.img | cut -d: -f1)
-sudo chmod a+rw ${loopdev}
-./build/chisai --format ${loopdev}
-echo ${loopdev} > loopdev.txt
--- scripts/umount_image.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-loopdev=$(cat loopdev.txt)
-sudo losetup -d ${loopdev}
-umount mount
