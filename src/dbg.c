deb6aa1d6b5f39c00ea368493c8a88d555a8971e
RinHizakura <s921975628@gmail.com>
2022-04-08 14:32:16 +0800

    Provide better interface to add subcommand

--- src/dbg.c
+++ src/dbg.c
@@ -9,8 +9,15 @@ static dbg_t *gDbg;
 
 typedef bool (*cmd_func)(int argc, char *argv[]);
 
-struct cmd_entry {
+struct opt_entry {
     struct list_head node;
+    char *name;
+    cmd_func op;
+};
+
+struct cmd_entry {
+    struct list_head node;  // the node for pending on dbg_t
+    struct list_head list;  // the list of option parameters
     char *name;
     char *description;
     cmd_func op;
@@ -22,6 +29,7 @@ static bool dbg_add_cmd(dbg_t *dbg, char *name, cmd_func op, char *description)
     if (!entry)
         return false;
     INIT_LIST_HEAD(&entry->node);
+    INIT_LIST_HEAD(&entry->list);
 
     entry->name = name;
     entry->description = description;
@@ -29,6 +37,42 @@ static bool dbg_add_cmd(dbg_t *dbg, char *name, cmd_func op, char *description)
 
     list_add_tail(&entry->node, &dbg->list);
     return true;
+}
+
+static bool __dbg_add_option(struct cmd_entry *cmd, char *name, cmd_func op)
+{
+    struct opt_entry *entry = malloc(sizeof(struct opt_entry));
+    if (!entry)
+        return false;
+    INIT_LIST_HEAD(&entry->node);
+
+    entry->name = name;
+    entry->op = op;
+
+    list_add_tail(&entry->node, &cmd->list);
+    return true;
+}
+
+static bool dgb_add_option(dbg_t *dbg,
+                           char *cmd_name,
+                           char *opt_name,
+                           cmd_func op)
+{
+    struct cmd_entry *entry = malloc(sizeof(struct cmd_entry));
+    if (!entry)
+        return false;
+
+    struct cmd_entry *item, *tmp;
+
+    list_for_each_entry_safe(item, tmp, &dbg->list, node)
+    {
+        if (!strcmp(item->name, cmd_name)) {
+            __dbg_add_option(item, opt_name, op);
+            return true;
+        }
+    }
+
+    return false;
 }
 
 static bool do_help(__attribute__((unused)) int argc,
@@ -59,25 +103,19 @@ static bool do_break(int argc, char *argv[])
     return target_set_breakpoint(&gDbg->target, addr);
 }
 
-static bool do_regs(int argc, char *argv[])
+static bool do_regs_read(int argc, char *argv[])
 {
+    size_t value;
+    char *reg_name = argv[2];
+
     if (argc != 3)
         return false;
 
-    bool ret = false;
-    char *sub_cmd = argv[1];
-    char *reg_name = argv[2];
-
-    if (strcmp(sub_cmd, "read") == 0) {
-        size_t value;
-        ret = target_get_reg(&gDbg->target, reg_name, &value);
-        if (!ret) {
-            printf("Unknown register name '%s'\n", reg_name);
-        } else {
-            printf("reg %s = %lx\n", reg_name, value);
-        }
+    bool ret = target_get_reg(&gDbg->target, reg_name, &value);
+    if (!ret) {
+        fprintf(stderr, "Unknown register name '%s'\n", reg_name);
     } else {
-        printf("Unknown subcommand '%s' of regs\n", sub_cmd);
+        printf("reg %s = %lx\n", reg_name, value);
     }
 
     return ret;
@@ -117,8 +155,9 @@ bool dbg_init(dbg_t *dbg, char *cmd)
     dbg_add_cmd(dbg, "help", do_help, "print me!");
     dbg_add_cmd(dbg, "cont", do_cont, "restart the stopped tracee process.");
     dbg_add_cmd(dbg, "break", do_break, "set breakpoint on tracee process.");
-    dbg_add_cmd(dbg, "regs", do_regs, "dump registers.");
 
+    dbg_add_cmd(dbg, "regs", NULL, "dump registers.");
+    dgb_add_option(dbg, "regs", "read", do_regs_read);
     linenoiseSetCompletionCallback(completion);
     return true;
 }
@@ -150,6 +189,31 @@ static char **dbg_parse_cmd(char *line, int *argc)
     return argv;
 }
 
+static bool dbg_match_subcmd(struct cmd_entry *item, int argc, char *argv[])
+{
+    bool ret = false;
+    char *sub_cmd = argv[1];
+    struct opt_entry *opt_item, *tmp;
+
+    if (argc < 2)
+        return false;
+
+    list_for_each_entry_safe(opt_item, tmp, &item->list, node)
+    {
+        if (!strcmp(opt_item->name, sub_cmd)) {
+            if (opt_item->op) {
+                ret = opt_item->op(argc, argv);
+                break;
+            }
+        }
+    }
+
+    if (!ret)
+        fprintf(stderr, "Sub-command '%s' not found or failed\n", sub_cmd);
+
+    return ret;
+}
+
 static bool dbg_match_cmd(dbg_t *dbg, int argc, char *argv[])
 {
     bool ret = false;
@@ -158,20 +222,23 @@ static bool dbg_match_cmd(dbg_t *dbg, int argc, char *argv[])
     list_for_each_entry_safe(item, tmp, &dbg->list, node)
     {
         if (!strcmp(item->name, argv[0])) {
-            ret = item->op(argc, argv);
-            goto cmd_found;
+            if (item->op) {
+                ret = item->op(argc, argv);
+            } else {
+                ret = dbg_match_subcmd(item, argc, argv);
+            }
+            break;
         }
     }
 
-    return false;
+    if (!ret)
+        fprintf(stderr, "Command '%s' not found or failed\n", argv[0]);
 
-cmd_found:
     return ret;
 }
 
 void dbg_run(dbg_t *dbg)
 {
-    bool ret;
     char *line;
     int argc;
     char **argv;
@@ -179,9 +246,7 @@ void dbg_run(dbg_t *dbg)
     while ((line = linenoise("(raid)")) != NULL) {
         linenoiseHistoryAdd(line);
         argv = dbg_parse_cmd(line, &argc);
-        ret = dbg_match_cmd(dbg, argc, argv);
-        if (!ret)
-            fprintf(stderr, "Command '%s' not found or failed\n", line);
+        dbg_match_cmd(dbg, argc, argv);
         linenoiseFree(line);
     }
 }
